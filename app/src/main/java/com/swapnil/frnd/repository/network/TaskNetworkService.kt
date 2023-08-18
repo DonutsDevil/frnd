2b0f21844e4e66e32d4906d28b1706ca12c1892a
Swapnil <swapnil.padaya2699@gmail.com>
2023-08-19 00:39:08 +0530

    Added Network Service interface

--- app/src/main/java/com/swapnil/frnd/repository/network/TaskNetworkService.kt
+++ app/src/main/java/com/swapnil/frnd/repository/network/TaskNetworkService.kt
@@ -1,5 +1,9 @@
 package com.swapnil.frnd.repository.network
 
-interface TaskNetworkService {
+import com.swapnil.frnd.model.Task
 
+interface TaskNetworkService {
+    suspend fun getTasks(userId: Int): List<Task>
+    suspend fun postTask(userId: Int, task: Task)
+    suspend fun deleteTask(userId: Int, taskId: Int)
 }
